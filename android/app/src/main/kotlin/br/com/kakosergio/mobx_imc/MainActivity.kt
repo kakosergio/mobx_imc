96f74787247c3023003b9d864b610921d0a001d9
kakosergio <sergioricardort@hotmail.com>
2023-05-04 07:05:34 -0300

    first commit

---
+++ android/app/src/main/kotlin/br/com/kakosergio/mobx_imc/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.kakosergio.mobx_imc
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/contador/contador_page.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:mobx_imc/contador/contador_controller.dart';
+
+class ContadorPage extends StatelessWidget {
+  final controller = ContadorController();
+  ContadorPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Meu Contador"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Observer(builder: (_) => Text(
+              '${controller.counter}',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),)
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: controller.increment,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
